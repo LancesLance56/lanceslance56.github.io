8729eeac9494a2510c0f776efa3c45d3eed6e786
LancesLance56 <lanceslance56@gmail.com>
2025-12-19 21:42:37 +0800

    images

--- js/components/Quiz.js
+++ js/components/Quiz.js
@@ -5,7 +5,9 @@ export default {
             quiz: [
                 { question: "What is the most popular festival in Binondo?", options: ["Lantern Festival", "Chinese New Year", "Dragon Boat", "Mooncake Festival"], correct: "Chinese New Year", selected: null, status: null },
                 { question: "Which color symbolizes good luck?", options: ["Blue", "White", "Red", "Black"], correct: "Red", selected: null, status: null },
-                { question: "What is a common staple food?", options: ["Dimsum", "Pizza", "Burger", "Tacos"], correct: "Dimsum", selected: null, status: null }
+                { question: "Which is a common Chinese Dish?", options: ["Dimsum", "Pizza", "Burger", "Tacos"], correct: "Dimsum", selected: null, status: null },
+                { question: "What region of China do most Tsinoys originate from?", options: ["Beijing", "Xi'An", "Yunnan", "Fujian"], correct: "Fujian", selected: null, status: null },
+                { question: "Around when was the earliest confirmed trade conducted between China and the Philippines?", options: ["~900s AD", "~200s BC", "~1500s AD", "~1200s AD"], correct: "~900s AD", selected: null, status: null },
             ]
         };
     },
